278e2bacea9c098cbe591a178bdd66509629e38f
LC70716 <contileonardo59@gmail.com>
2024-06-13 22:13:49 +0200

    contrast code and updated matlab code

--- ContrastMax.py
+++ ContrastMax.py
@@ -1,12 +1,14 @@
 #%%
+
 import numpy as np
 from scipy.optimize import shgo
 from scipy.optimize import Bounds
+import matplotlib.pyplot as plt
 
 def my_function(x): 
     x1 = x[0] #x1 = TR, x2 = TE/2, the 2 are placeholder values for M0A and M0B
     x2 = x[1]
-    return (2*(1-np.exp(-x1/80)*2*np.exp((x2/30)-1))*np.exp(-x2/30) - 2*(1-np.exp(-x1/1000)*2*np.exp((x2/500)-1))*np.exp(-x2/500))
+    return ((7.57e05)*(1-np.exp(-x1/78.9530)*(2*np.exp(x2/35.8829)-1))*np.exp(-2*x2/35.8829) - (9.03e05)*(1-np.exp(-x1/1010.5)*(2*np.exp(x2/356.7618)-1))*np.exp(-2*x2/356.7618))
 
 def inverted_function(x):
     if my_function(x) >= 0 :
@@ -14,47 +16,21 @@ def inverted_function(x):
     else :
         return -1*1/my_function(x)
 
-initial_guess = np.array([200.0, 10.0])
+initial_guess = np.array([100.0, 5])
 
-bounds = Bounds([100, 5], [2000, 500])
-
-results = dict()
-
-results['shgo_sobol'] = shgo(inverted_function, bounds, n=1000, iters=5,sampling_method='sobol')
-
-results['shgo_sobol'] #results obtained with this definition of contrast make little sense
-
-# %%
-import numpy as np
-from scipy.optimize import shgo
-from scipy.optimize import Bounds
-
-
-def my_function(x): 
-    x1 = x[0] #x1 = TR, x2 = TE/2, the 2 are placeholder values for M0A and M0B
-    x2 = x[1]
-    SA = 2*(1-np.exp(-x1/80)*2*np.exp((x2/30)-1))*np.exp(-x2/30)
-    SB = 2*(1-np.exp(-x1/1000)*2*np.exp((x2/500)-1))*np.exp(-x2/500)
-    AbsSA = SA
-    AbsDiff = SA-SB
-    if SA < 0 :
-        AbsSA = -1*SA
-    if AbsDiff < 0 :
-        AbsDiff = -1*AbsDiff
-    return AbsDiff/AbsSA
-
-def inverted_function(x):
-    if my_function(x) >= 0 :
-        return 1/my_function(x)
-
-initial_guess = np.array([200.0, 10.0])
-
-bounds = Bounds([100, 5], [10000, 500])
+bounds = Bounds([100, 2.5], [1300, 7.5])
 
 results = dict()
 
-results['shgo_sobol'] = shgo(inverted_function, bounds, n=1000, iters=5,sampling_method='sobol')
+results['shgo_sobol'] = shgo(inverted_function, bounds, sampling_method='sobol',n=10000, iters =30)
 
-results['shgo_sobol']
+results['shgo_sobol'] 
+
+x=results['shgo_sobol'].xl
+y = results['shgo_sobol'].funl
+print(x)
+print(y)
+print(np.shape(y))
+plt.plot(y)
 
 # %%
--- Lab1_T1&T2_weighting/data/T1T2fromUpen.m
+++ Lab1_T1&T2_weighting/data/T1T2fromUpen.m
@@ -1,4 +1,5 @@
 T = readtable('CPMGalbume1B.dat');
+name="cpmg albume"
 sumDistrT2 = sum(T{:,"Sig_Np"},'omitnan');
 weightsT2 = T{:,"Sig_Np"}./sumDistrT2;
 T2 = sum(T{:,"T"}.*weightsT2(:),'omitnan')
@@ -24,6 +25,7 @@ set(gca,'yscale','log')
 hold off
 
 T = readtable('CPMGtuorlo1B.dat');
+name="cpmg tuorlo"
 sumDistrT2 = sum(T{:,"Sig_Np"},'omitnan');
 weightsT2 = T{:,"Sig_Np"}./sumDistrT2;
 T2 = sum(T{:,"T"}.*weightsT2(:),'omitnan')
@@ -49,6 +51,7 @@ set(gca,'yscale','log')
 hold off
 
 T = readtable('IRalbumeH.dat');
+name="IR albume"
 sumDistrT1 = sum(T{:,"Sig_Np"},'omitnan');
 weightsT1 = T{:,"Sig_Np"}./sumDistrT1;
 T1 = sum(T{:,"T"}.*weightsT1(:),'omitnan')
@@ -74,6 +77,7 @@ mdl = fitnlm(time,signal,fnc,B0)
 hold off
 
 T = readtable('IRtuorloH.dat');
+name="IR tuorlo"
 sumDistrT1 = sum(T{:,"Sig_Np"},'omitnan');
 weightsT1 = T{:,"Sig_Np"}./sumDistrT1;
 T1 = sum(T{:,"T"}.*weightsT1(:),'omitnan')
